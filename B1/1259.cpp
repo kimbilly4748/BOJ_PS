a7e812bd8c5d897c66188be7d0f2328037c70793
TUNAMAYO <62492740+kimbilly4748@users.noreply.github.com>
2024-03-11 12:50:54 +0900

    0311

---
+++ B1/1259.cpp
@@ -0,0 +1,26 @@
+//#include "iostream"
+//
+//using namespace std;
+//
+//int main()
+//{
+//    string a;
+//    cin >> a;
+//    if(a == "0")
+//    {
+//        return 0;
+//    }
+//    else
+//    {
+//        for(int i=0; i<a.size()/2; i++)
+//        {
+//            if(a[i] != a[a.size()-i-1])
+//            {
+//                cout << "no" << endl;
+//                return main();
+//            }
+//        }
+//        cout << "yes" << endl;
+//        return main();
+//    }
+//}
